217894fa3404edc1fadd85899ed4a40f7b9867f1
sorryma3er <whc0713air@gmail.com>
2025-12-18 23:39:56 +0700

    Added basic properties of trip and two-way binding to UI.

--- trip-planner/src/app/plan/plan.ts
+++ trip-planner/src/app/plan/plan.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+type Style = 'Relax' | 'Adventure' | 'Foodie' | 'Culture';
 
 @Component({
   selector: 'app-plan',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './plan.html',
   styleUrl: './plan.css',
 })
 export class Plan {
+  // trip basic info fill in & two way binding
+  destination: string = '';
+  startDate: Date = new Date();
+  endDate: Date = new Date();
+  budget: number = 0;
+  travelers: number = 0;
+  style: Style = 'Relax';
+
 
 }
