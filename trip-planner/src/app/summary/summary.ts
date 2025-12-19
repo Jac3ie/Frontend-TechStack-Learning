95e0de78a0a09714109c7b6bbccb38b3dfee7fc8
sorryma3er <whc0713air@gmail.com>
2025-12-19 14:49:21 +0700

    Added UI for summary page that use interpolation to load state.value; and display any stored plan to user.

--- trip-planner/src/app/summary/summary.ts
+++ trip-planner/src/app/summary/summary.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { Trip } from '../trip';
 
 @Component({
   selector: 'app-summary',
-  imports: [],
+  standalone: true,
   templateUrl: './summary.html',
   styleUrl: './summary.css',
 })
 export class Summary {
+  constructor(public trip: Trip) {}
 
+  get state() {
+    return this.trip.getState();
+  }
+
+  get planTotal() {
+    return this.state.planItems.length;
+  }
+
+  get planDone() {
+    return this.state.planItems.filter(p => p.done).length;
+  }
 }
